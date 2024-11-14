c3daa87e0122fc1907a892582df0b4e0140020c9
nannipy <pernazza.1936239@studenti.uniroma1.it>
2024-11-14 16:39:33 +0100

    icon 3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -62,7 +62,7 @@ export default function RootLayout({
     <html lang="en" className="bg-black text-xl">
       <head>
         <link rel="manifest" href="/manifest.json" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/public/favicon.ico" />
       </head>
       <body className="bg-black ">
               {children}
